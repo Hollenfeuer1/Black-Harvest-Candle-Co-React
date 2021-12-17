be055d665dcc4fb40f40ecdd3b519e9e4ed878ba
Alyssa Cabello <48364178+Hollenfeuer1@users.noreply.github.com>
2021-12-17 08:50:54 -0600

    Add files via upload

---
+++ Pages/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactUs = () => {
+
+    return (
+        <div>
+            CONTACT
+        </div>
+    );
+  }
+  
+  export default ContactUs;
---
+++ Pages/FAQ.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const faq = () => {
+
+    return (
+        <div>
+            FAQ
+        </div>
+    );
+  }
+  
+  export default faq;
